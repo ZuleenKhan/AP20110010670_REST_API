d1a713ee2a21474c445f3bdebe0702ac18f856d7
ZuleenKhan <zuleen.khan670@gmail.com>
2023-09-08 16:24:54 +0530

    BAJAJ

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
 const express= require('express') ; 
+
 const app = express() ; 
 const PORT  = 8080; 
 
 app.use(express.json()) ; 
 
-app.get('/tshirts',(req,res)=>{
-    res.status(200).send({
-         logo: "34" 
+app.get('/',(req,res)=>{
+       res.status(200).send({
+            operation_code: 1 
+       })
+});
 
-    })
-
-}) ; 
-
-app.post('/tshirts/:id', (req,res)=>{
+app.post('/bfhl', (req,res)=>{
       
       const {id} = req.params ; 
-      const {logo} = req.body ; 
-      if(!logo) res.send("No logo is there !! ") ; 
-      else res.send(`Your logo is ${logo} & id is : ${id}`) ; 
+      const {data} = req.body ;
+      function isNumber(str) {
+        return !isNaN(parseFloat(str)) && isFinite(str);
+      }
+      function isAlphabetCharacter(str) {
+        return /^[a-zA-Z]$/.test(str);
+      }
+      const alphabetArray = data.filter((str) => isAlphabetCharacter(str));
+      // Find numbers in the array
+      const numbersArray = data.filter((str) => isNumber(str));
+
+      const highestAlphabet = data.reduce((max, current) => {
+        return current > max ? current : max;
+      }, 'a');
+
+      if(!data) res.send("No data is there !! ") ; 
+      else { 
+      res.status(200).send(
+        {
+            is_success: `true`,
+            user_id: `Zuleen_Khan_AP20110010670`,
+            email : `zuleen_khan@srmap.edu.in`,
+            roll_number: `AP20110010670`,
+            numbers: numbersArray, 
+            alphabets: alphabetArray,
+            highest_alphabet: highestAlphabet 
+            
+        });
+    }
 
 }) ; 
 
@@ -25,3 +50,6 @@ app.listen(PORT, ()=>{
    console.log(`App running on port: ${PORT} ` ) ; 
 
 })  ; 
+
+//app.use('/netlify/functions/api',router) ; 
+// module.exporrts.handler = serverless(app) ; 
